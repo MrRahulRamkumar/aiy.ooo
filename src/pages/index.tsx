eba133732063771f4ffbc2b40dc3a995c5657ff3
MrRahulRamkumar <rahulawesomeramkumar@gmail.com>
2023-04-28 19:06:54 +0530

    Added input component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,6 +5,7 @@ import { signIn, signOut, useSession } from "next-auth/react";
 import { Button } from "@/components/ui/button";
 
 import { api } from "@/lib/api";
+import { Input } from "@/components/ui/input";
 
 const Home: NextPage = () => {
   const hello = api.example.hello.useQuery({ text: "from tRPC" });
@@ -17,13 +18,16 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className="flex min-h-screen flex-col items-center justify-center">
-        <Button>Button</Button>
         <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
           ayiooo
         </h1>
-        <p className="leading-7 [&:not(:first-child)]:mt-6">
+        <p className="leading-7 [&:not(:first-child)]:mt-2">
           A blazingly fast link shortener
         </p>
+        <div className="mt-6 flex w-full max-w-sm items-center space-x-2">
+          <Input type="text" placeholder="https://bing.com" />
+          <Button type="submit">Create</Button>
+        </div>
       </main>
     </>
   );

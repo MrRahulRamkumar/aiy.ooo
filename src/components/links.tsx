74ae624e8443d7556d85906f7781afa3309a806e
Rahul Ramkumar <rahulawesomeramkumar@gmail.com>
2024-03-30 06:14:42 +0530

    Refactor link list layout and remove unnecessary code

--- src/components/links.tsx
+++ src/components/links.tsx
@@ -39,27 +39,22 @@ const LinkList = () => {
   }
 
   return (
-    <div className="mt-6 grid">
-      <ul role="list" className="divide-y">
+    <div className="mt-6">
+       <ul className="divide-y">
         {links.map((link) => (
           <li key={link.id} className="flex justify-between gap-x-6 py-5">
             <div className="flex gap-x-4">
               <div className="min-w-0 flex-auto">
-                <p className="text-md w-40 overflow-hidden truncate font-bold leading-6 sm:w-80">
+                <p className="text-md overflow-hidden truncate font-bold leading-6">
                   {link.slug}
                 </p>
-                <p className="mt-1 w-40 overflow-hidden truncate text-xs leading-5 opacity-50 sm:w-80">
-                  {link.url}
+                <p className="mr-2 truncate text-ellipsis text-center text-sm font-medium leading-5 opacity-50">
+                  {timeAgoFormatter(link.createdAt)}
                 </p>
               </div>
             </div>
             <div className="flex flex-col items-end">
-              <div className="flex flex-row items-center justify-center">
-                <p className="mr-2 truncate text-ellipsis text-center text-sm font-medium leading-5 opacity-50">
-                  {timeAgoFormatter(link.createdAt)}
-                </p>
                 <SettingsMenu link={link} />
-              </div>
             </div>
           </li>
         ))}
@@ -74,9 +69,7 @@ export const LinkSheet = () => {
       <SheetTrigger asChild>
         <Button>View your Links</Button>
       </SheetTrigger>
-      <SheetContent
-        className="overflow-auto"
-      >
+      <SheetContent>
         <SheetHeader>
           <SheetTitle>Your links</SheetTitle>
           <SheetDescription>View and edit your links</SheetDescription>
